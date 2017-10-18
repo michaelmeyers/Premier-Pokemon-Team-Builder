826da01200a986867acd78a562c5545dcca2c76c
michaelmeyers <31745265+michaelmeyers@users.noreply.github.com>
2017-10-18 16:04:59 -0600

    Initial Commit

---
+++ PremierTeamBuilder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PremierTeamBuilder
+//
+//  Created by Michael Meyers on 10/18/17.
+//  Copyright © 2017 Michael Meyers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
