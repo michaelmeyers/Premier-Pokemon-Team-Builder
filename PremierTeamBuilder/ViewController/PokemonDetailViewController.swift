c28c37787deb08b9b5d7316479a8c25c6a1f61c8
michaelmeyers <31745265+michaelmeyers@users.noreply.github.com>
2017-10-25 12:51:41 -0600

    Added properties and Outlets to PokemonDetailVC.  Set it up to display pokemon information.

--- PremierTeamBuilder/ViewController/PokemonDetailViewController.swift
+++ PremierTeamBuilder/ViewController/PokemonDetailViewController.swift
@@ -12,11 +12,45 @@ class PokemonDetailViewController: UIViewController {
     
     // MARK: - Properties
     var pokemon: Pokemon?
+    
+    // MARK: - Outlets
+    @IBOutlet weak var pokemonImageView: UIImageView!
+    @IBOutlet weak var natureLabel: UILabel!
+    @IBOutlet weak var abilityLabel: UILabel!
+    @IBOutlet weak var itemLabel: UILabel!
+    @IBOutlet weak var type1Label: UILabel!
+    @IBOutlet weak var type2Label: UILabel!
+    @IBOutlet weak var hpLabel: UILabel!
+    @IBOutlet weak var attackLabel: UILabel!
+    @IBOutlet weak var defenseLabel: UILabel!
+    @IBOutlet weak var spAtkLabel: UILabel!
+    @IBOutlet weak var spDefLabel: UILabel!
+    @IBOutlet weak var speedLabel: UILabel!
 
+    // MARK: - ViewDidLoad()
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        if let pokemon = pokemon {
+            guard let data = pokemon.imageData,
+                let pokeImage = UIImage(data: data) else {return}
+            pokemonImageView.image = pokeImage
+            natureLabel.text = pokemon.nature.rawValue
+            abilityLabel.text =  pokemon.chosenAbility
+            itemLabel.text = pokemon.item
+            type1Label.text = pokemon.type1.rawValue
+            if let type2 = pokemon.type2 {
+                type2Label.text = type2.rawValue
+            } else {
+                type2Label.isHidden = true
+            }
+            hpLabel.text = "\(String(describing: pokemon.hpStat))"
+            attackLabel.text = "\(String(describing: pokemon.attackStat))"
+            defenseLabel.text = "\(String(describing: pokemon.defenseStat))"
+            spAtkLabel.text = "\(String(describing: pokemon.spAttackStat))"
+            spDefLabel.text = "\(String(describing: pokemon.spDefenseStat))"
+            speedLabel.text = "\(String(describing: pokemon.speedStat))"
+            
+        }
     }
 
     override func didReceiveMemoryWarning() {
